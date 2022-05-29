e578203983d3f666666d7744d9cd627de1226859
Navneet <hunnyhunnyhunny@gmail.com>
2022-05-29 12:58:58 +0530

    Added Interceptor for loading screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { delay, Observable } from 'rxjs';
 import { Coffee } from './model/coffee.model';
+import { LoadingService } from './services/loading.service';
 import { fromRoot } from './store';
 import { RootState } from './store/reducer';
 
@@ -16,17 +17,22 @@ export class AppComponent implements OnInit, OnDestroy {
   data$: Observable<Coffee[]>;
   coffeeList: Coffee[] = [];
   visibleCoffeeList: Coffee[] = [];
+  loading = false;
 
 
   @ViewChild('paginator') paginator: MatPaginator;
 
-  constructor(private store: Store<{ rootState: RootState }>) {
+  constructor(private store: Store<{ rootState: RootState }>,
+    private _loading: LoadingService
+  ) {
     this.error$ = this.store.select(fromRoot.getStateError);
     this.data$ = this.store.select(fromRoot.getStateSelectedData);
   }
 
 
   ngOnInit(): void {
+    this.listenToLoading();
+
     this.store.dispatch(fromRoot.CoffeePageInit());
     this.store.dispatch(fromRoot.ApiGetMockData());
     this.data$.subscribe((data: Coffee[]) => {
@@ -44,6 +50,19 @@ export class AppComponent implements OnInit, OnDestroy {
   onPageChange($event: PageEvent): void {
     this.visibleCoffeeList = this.coffeeList.slice($event.pageIndex * $event.pageSize,
       $event.pageIndex * $event.pageSize + $event.pageSize);
+  }
+
+
+  /**
+   * Listen to the loadingSub property in the LoadingService class. This drives the
+   * display of the loading spinner.
+   */
+  listenToLoading(): void {
+    this._loading.loadingSub
+      .pipe(delay(0)) // This prevents a ExpressionChangedAfterItHasBeenCheckedError for subsequent requests
+      .subscribe((loading) => {
+        this.loading = loading;
+      });
   }
 
   ngOnDestroy() {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { FooterComponent } from './components/footer/footer.component';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { EffectsModule } from '@ngrx/effects'
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { fromRoot } from './store';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { CardComponent } from './components/card/card.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { HttpRequestInterceptor } from './http-request-interceptor';
 
 @NgModule({
   declarations: [
@@ -30,8 +32,15 @@ import { CardComponent } from './components/card/card.component';
     EffectsModule.forRoot([fromRoot.RootEffects]),
     BrowserAnimationsModule,
     MatPaginatorModule,
+    MatProgressSpinnerModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpRequestInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
