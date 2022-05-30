3b2a73f69febba6c28abf92fa03be1adc8081558
Navneet <hunnyhunnyhunny@gmail.com>
2022-05-30 18:25:02 +0530

    Style refactor

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,25 +1,33 @@
 import { NgModule } from '@angular/core'
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDividerModule } from '@angular/material/divider'
+import { MatExpansionModule } from '@angular/material/expansion'
 
 @NgModule({
   imports: [
     MatPaginatorModule,
     MatProgressSpinnerModule,
     MatDialogModule,
-    MatListModule,
-    MatButtonModule
+    MatCardModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatDividerModule,
+    MatExpansionModule
   ],
   exports: [
     MatPaginatorModule,
     MatProgressSpinnerModule,
     MatDialogModule,
-    MatListModule,
-    MatButtonModule
+    MatCardModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatDividerModule,
+    MatExpansionModule
   ]
 })
 export class MaterialModule { }
